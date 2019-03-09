469c083f618b6c52c83b0b90d6b17d915176f58a
Max Taggart <max.taggart@healthcatalyst.com>
2019-03-09 00:24:35 +0000

    Made installer Get-PythonInstallerUrl more flexible.

--- Functions/Get-PythonInstallerUrl.ps1
+++ Functions/Get-PythonInstallerUrl.ps1
@@ -1,10 +1,5 @@
 function Get-PythonInstallerUrl($Version){
     # python versions 2.7.x do not have executable installers and must be installed manually
-    $installerURLMap = @{
-        '3.7.2' = "https://www.python.org/ftp/python/3.7.2/python-3.7.2-amd64.exe";
-        '3.6.8' = "https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe";
-    }
-
-    return $installerURLMap[$Version]
+    return "https://www.python.org/ftp/python/$Version/python-$Version-amd64.exe"
 }
 
