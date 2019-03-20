0bef084faa6ed47b98309e80b42542e827bba1be
MaxTaggart <max.taggart@gmail.com>
2019-03-20 22:48:03 +0000

    Added Force arguments to test and Force argument to Restore-PythonUtiltiesConfigDefaults.

--- Functions/Restore-PythonUtilitiesConfigDefaults.ps1
+++ Functions/Restore-PythonUtilitiesConfigDefaults.ps1
@@ -1,6 +1,13 @@
 . "$PSScriptRoot\ConfigGettersAndSetters.ps1"
 
-function Restore-PythonUtilitiesConfigDefaults(){
-    Set-PythonInstallRoot -Path "C:\PythonInstallations\"
-    Set-VirtualEnvironmentRoot -Path "C:\PythonVirtualEnvironments\"
+function Restore-PythonUtilitiesConfigDefaults([switch]$Force){
+    if ($Force){
+        Set-PythonInstallRoot -Path "C:\PythonInstallations\" -Force
+        Set-VirtualEnvironmentRoot -Path "C:\PythonVirtualEnvironments\" -Force
+    }
+    else {
+        Set-PythonInstallRoot -Path "C:\PythonInstallations\"
+        Set-VirtualEnvironmentRoot -Path "C:\PythonVirtualEnvironments\"
+    }
+    
 }
