5db2a07de4b06dc792978cf5a5a6961c23ba39c3
MaxTaggart <max.taggart@gmail.com>
2019-03-27 20:52:48 +0000

    Updated call to Remove-PythonInstallation to use new name.

--- Tests/Tests.tests.ps1
+++ Tests/Tests.tests.ps1
@@ -48,7 +48,7 @@ Describe "Installing new python versions and creating virtual environments" -Tag
         Test-Path -Path "$venvRoot\$venvName-$testVersion\python.exe" | Should -BeFalse
     }
 
-    Remove-PythonInstallation -Version $testVersion
+    Uninstall-Python -Version $testVersion
 
     It "Should reset the config to the default value" {
         Restore-PythonUtilitiesConfigDefaults -Force
