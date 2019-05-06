22ef90e1e89ada9aa8ed40dc60e114a908c79f33
MaxTaggart <max.taggart@gmail.com>
2019-05-06 21:26:41 +0000

    Switched to yaml-based build pipeline and added more comprehensive tests.

--- Functions/New-PythonVirtualEnvironment.ps1
+++ Functions/New-PythonVirtualEnvironment.ps1
@@ -3,7 +3,7 @@
 function New-PythonVirtualEnvironment([string]$ShortVersion, [string]$Name){
 $Null = @(
     if ($ShortVersion.split('.').count -eq 3){
-        Write-Warning "It is only necessary to specify the major.minor version of the python version that you wish to use to create the virtual environment. This is because only one patch version of a given major.minor version can be installed at at time, so the patch version used for the venv will be whatever is currently installed in the location returned by `Get-PythonInstallRoot`."
+        Write-Warning "It is only necessary to specify the major.minor version of the python version that you wish to use to create the virtual environment. This is because only one patch version of a given major.minor version can be installed at at time, so the patch version used for the venv will be whatever is currently installed in the location returned by 'Get-PythonInstallRoot'."
         $ShortVersion = Convert-FullVersionToShortVersion $ShortVersion
     }
     $installRoot = Get-PythonInstallRoot
@@ -12,10 +12,10 @@ $Null = @(
     $virtualEnvPath = [io.path]::Combine($virtualenvRoot, "$Name-$ShortVersion")
 
     if(!(Test-ShortVersionIsInstalled $ShortVersion)){
-        throw "Python version $ShortVersion is not installed. Please use `Install-Python` to install it and then re-run this command."
+        throw "Python version $ShortVersion is not installed. Please use 'Install-Python' to install it and then re-run this command."
     }
     if (Test-Path -Path $virtualEnvPath){
-        throw "A virtual environment with this name and python version already exists. Please use `Get-PythonVirtualEnvironments` to see a list of existing environments."
+        throw "A virtual environment with this name and python version already exists. Please use 'Get-PythonVirtualEnvironments' to see a list of existing environments."
     }
 
     Write-Host "Creating new virtual environment for Python version $ShortVersion at $virtualEnvPath."
